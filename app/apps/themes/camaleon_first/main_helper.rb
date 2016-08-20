b0822f11a832d0911cdf251afe3196fead096b4e
Zachary Espiritu <me@zacharyespiritu.com>
2016-08-20 15:30:30 -0400

    Reformat main_helper to be more readable

--- app/apps/themes/camaleon_first/main_helper.rb
+++ app/apps/themes/camaleon_first/main_helper.rb
@@ -21,9 +21,18 @@ module Themes::CamaleonFirst::MainHelper
     group.add_field({"name"=>"Maximum of items", "slug"=>"home_qty"}, {field_key: "numeric", default_value: 6})
 
     group = theme.add_field_group({name: "Footer", slug: "footer"})
-    group.add_field({"name"=>"Column Left", "slug"=>"footer_left"}, {field_key: "editor", translate: true, default_value: "<h4>My Bunker</h4><p>Some Address 987,<br> +34 9054 5455, <br> Madrid, Spain. </p>"})
-    group.add_field({"name"=>"Column Center", "slug"=>"footer_center"}, {field_key: "editor", translate: true, default_value: "<h4>My Links</h4> <p><a href='#'>Dribbble</a><br> <a href='#'>Twitter</a><br> <a href='#'>Facebook</a></p>"})
-    group.add_field({"name"=>"Column Right", "slug"=>"footer_right"}, {field_key: "editor", translate: true, default_value: "<h4>About Theme</h4><p>This cute theme was created to showcase your work in a simple way. Use it wisely.</p>"})
+    group.add_field({"name"=>"Column Left", "slug"=>"footer_left"}, {
+      field_key: "editor",
+      translate: true,
+      default_value: "<h4>My Bunker</h4><p>Some Address 987,<br> +34 9054 5455, <br> Madrid, Spain. </p>"})
+    group.add_field({"name"=>"Column Center", "slug"=>"footer_center"}, {
+      field_key: "editor",
+      translate: true,
+      default_value: "<h4>My Links</h4> <p><a href='#'>Dribbble</a><br> <a href='#'>Twitter</a><br> <a href='#'>Facebook</a></p>"})
+    group.add_field({"name"=>"Column Right", "slug"=>"footer_right"}, {
+      field_key: "editor",
+      translate: true,
+      default_value: "<h4>About Theme</h4><p>This cute theme was created to showcase your work in a simple way. Use it wisely.</p>"})
 
   end
 
